8a20a75f133a2044e4de80a42885487be130de81
KusalNayanajith99 <kusalnayanajith88@gmail.com>
2025-08-10 12:48:15 +0530

    Initial commit of my project

---
+++ assets/assets.js
@@ -0,0 +1,118 @@
+import user_image from './user-image.png';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    firebase,
+    figma,
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark
+};
+
+export const workData = [
+    {
+        title: 'Vehicle Valuation System',
+        description: 'Web & Mobile Application',
+        bgImage: '/miracularz.png',
+    },
+    {
+        title: 'Online Food Store System',
+        description: 'Frontend Web Application',
+        bgImage: '/Food-store.png',
+    },
+    {
+        title: 'Patient-Management System',
+        description: 'Backend Web Application',
+        bgImage: '/patient-management.png',
+    },
+    {
+        title: ' Intelligent Person Tracking System',
+        description: 'AI based project',
+        bgImage: '/Person-tracking.png',
+    },
+]
+
+export const serviceData = [
+    { icon: assets.web_icon, title: 'Backend Development', description: 'Building robust, scalable, and secure server-side applications and APIs to power dynamic experiences.', link: '' },
+    { icon: assets.web_icon, title: 'Frontend Development', description: 'Crafting intuitive and responsive user interfaces using modern frameworks and technologies.', link: '' },
+    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Developing native or cross-platform mobile applications for iOS and Android devices.', link: '' },
+    { icon: assets.web_icon, title: 'Cloud & Hosting Solutions', description: 'Deploying and managing applications on reliable cloud platforms, ensuring high availability and performance.', link: '' },
+]
+
+export const infoList = [
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'University of Moratuwa', description: 'B.Sc. (Hons) in Information Technology' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Matara Central College', description: ' G.C.E. Advanced Level G.C.E. Ordinary Level' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Technologies', description: '.NET, Springboot, Angular, React Js, Python, Next Js ' }
+    
+];
+
+export const toolsData = [
+    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git
+];
